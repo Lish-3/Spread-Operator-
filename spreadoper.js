fca722ec9a9a60669045c1d1538fe56d574c1fc7
Alishba Malik <muneeza336@gmail.com>
2024-10-13 18:34:33 +0500

    code file added

---
+++ spreadoper.js
@@ -0,0 +1,7 @@
+function party(a, b){
+    guest =['Aisha','Bob','Alics','Neil'];
+    newguest =['Hamro','Zeenar','Roger'];
+    totalguests= [guest,...newguest];
+    console.log(totalguests);
+}
+party();
